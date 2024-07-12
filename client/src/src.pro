11d8d73df0cb63080bee78803c578907ca07fba3
45gfg9 <45gfg9@45gfg9.net>
2024-07-12 17:16:48 +0800

    Add websockets code to project

--- client/src/src.pro
+++ client/src/src.pro
@@ -1,24 +1,28 @@
 TEMPLATE = app
-QT += core gui widgets sql
+QT += core gui widgets sql websockets
 CONFIG += c++11 debug
 HEADERS = \
     login/login_view.h \
     login/login_viewmodel.h \
     login/login_model.h \
+    websocket/websocket.h \
 
 SOURCES = \
     main.cpp \
     login/login_view.cpp \
     login/login_viewmodel.cpp \
     login/login_model.cpp \
+    websocket/websocket.cpp \
 
 RESOURCES += \
     resources.qrc
 
+DEFINES += MSGPACK_NO_BOOST
 LIBS += ../third_party/qt-material-widgets/components/libcomponents.a
 INCLUDEPATH += \
     ../third_party/qt-material-widgets/components/ \
-    ../third_party/qt-material-widgets/components/lib
+    ../third_party/qt-material-widgets/components/lib \
+    ../third_party/msgpack/include \
 
 TARGET = client.exe
 PRE_TARGETDEPS += ../third_party/qt-material-widgets/components/libcomponents.a
--- client/src/websocket/websocket.h
+++ client/src/websocket/websocket.h
@@ -4,37 +4,38 @@
 #include <QThread>
 #include <QWebSocket>
 
-class WebSocketThread : public QThread {
+class WebSocketThread : public QThread
+{
     Q_OBJECT
 
 public:
-    explicit WebSocketThread(QObject *parent = nullptr);
+    explicit WebSocketThread(QObject* parent = nullptr);
     ~WebSocketThread();
 
-    void connectSocket(const QString &wsUrl);
+    void connectSocket(const QString& wsUrl);
     void disconnectSocket();
-    void sendMessage(const QString &message);
+    void sendMessage(const QString& message);
     QAbstractSocket::SocketState state() const;
     void run() override;
 
 public slots:
     void onConnected();
     void onDisconnected();
-    void onMessageReceived(const QByteArray &message);
+    void onMessageReceived(const QByteArray& message);
 
     // LoginProxyModel
-    void on_login(const QString &_username, const QString &password);
+    void on_login(const QString& _username, const QString& password);
 
-    void on_send(/* type todo */);
- 
+    // void on_send(/* type todo */);
+
 signals:
-    void messageReceived(const QByteArray &message);
+    void messageReceived(const QByteArray& message);
 
     void loginSuccess();
     void loginFail();
 
 private:
-    QWebSocket *_socket;
+    QWebSocket* _socket;
     QUrl _wsUrl;
 };
 
