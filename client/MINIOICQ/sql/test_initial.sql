f683c7d5bcaa5a94aab7b11ea25f97f8aa616358
GoPoux <suyucheng5@gmail.com>
2024-07-14 01:27:58 +0800

    Fix bugs in ListModel and test_initial.sql

--- client/MINIOICQ/sql/test_initial.sql
+++ client/MINIOICQ/sql/test_initial.sql
@@ -5,10 +5,10 @@ INSERT INTO chats VALUES (1, 'chat1', null, 1, '2021-01-01 00:01:00', '2021-01-0
 INSERT INTO chats VALUES (2, 'chat2', null, 2, '2021-01-01 00:01:00', '2021-01-01 00:00:00');
 INSERT INTO chats VALUES (3, 'chat3', null, 1, '2021-01-01 00:01:00', '2021-01-01 00:00:00');
 
-INSERT INTO messages VALUES (1, 1, 'test_message1_from_user1_to_user2', '2021-01-01 00:00:00', 1);
-INSERT INTO messages VALUES (1, 2, 'test_message2_from_user2_to_user1', '2021-01-01 00:02:00', 2);
-INSERT INTO messages VALUES (2, 1, 'test_message1_from_user2_in_group', '2021-01-01 00:00:00', 2);
-INSERT INTO messages VALUES (2, 2, 'test_message2_from_user1_in_group', '2021-01-01 00:02:00', 1);
+INSERT INTO messages VALUES (1, 1, 'text/plain', 'test_message1_from_user1_to_user2', '2021-01-01 00:00:00', 1);
+INSERT INTO messages VALUES (1, 2, 'text/plain', 'test_message2_from_user2_to_user1', '2021-01-01 00:02:00', 2);
+INSERT INTO messages VALUES (2, 1, 'text/plain', 'test_message1_from_user2_in_group', '2021-01-01 00:00:00', 2);
+INSERT INTO messages VALUES (2, 2, 'text/plain', 'test_message2_from_user1_in_group', '2021-01-01 00:02:00', 1);
 
 INSERT INTO joins VALUES (2, 1, '2021-01-01 00:00:00');
 INSERT INTO joins VALUES (2, 2, '2021-01-01 00:00:00');
--- client/src/list/list_model.cpp
+++ client/src/list/list_model.cpp
@@ -17,8 +17,60 @@ void ListModel::setDatabase(QSqlDatabase db)
     if (!tables.contains("users"))
     {
         qDebug() << "Table users not found, creating tables";
-        QSqlQuery createTable(db);
-        createTable.exec(".read MINIOICQ/sql/client.sql");
+        QSqlQuery initdb(db);
+        initdb.exec("CREATE TABLE IF NOT EXISTS users ("
+                        "uid INT PRIMARY KEY,"
+                        "name VARCHAR NOT NULL,"
+                        "avatar BLOB"
+                        ");");
+        initdb.exec("CREATE TABLE IF NOT EXISTS chats ("
+                        "cid INT PRIMARY KEY,"
+                        "name VARCHAR NOT NULL,"
+                        "avatar BLOB,"
+                        "oid INT NOT NULL REFERENCES users(uid),"
+                        "last_view TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+                        "start_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+                        ");");
+        initdb.exec("CREATE TABLE IF NOT EXISTS messages ("
+                        "cid INT NOT NULL,"
+                        "mid INT NOT NULL,"
+                        "mtype VARCHAR NOT NULL,"
+                        "message BLOB NOT NULL,"
+                        "send_time TIMESTAMP NOT NULL,"
+                        "uid INT NOT NULL,"
+                        "PRIMARY KEY (cid, mid)"
+                        ");");
+        initdb.exec("CREATE TABLE IF NOT EXISTS joins ("
+                        "cid INT NOT NULL,"
+                        "uid INT NOT NULL,"
+                        "join_time TIMESTAMP NOT NULL,"
+                        "PRIMARY KEY (cid, uid)"
+                        ");");
+        initdb.exec("CREATE VIEW IF NOT EXISTS chat_list_view AS "
+                        "WITH latest_messages AS ("
+                        "SELECT c.cid, c.name, c.avatar, m.mtype, m.message, m.send_time,"
+                        "RANK() OVER (PARTITION BY c.cid ORDER BY m.send_time DESC) AS rank "
+                        "FROM chats c LEFT JOIN messages m ON c.cid = m.cid),"
+                        "messages_after_last_view AS ("
+                        "SELECT c.cid, COUNT(m.mid) AS msg_count_after_last_view "
+                        "FROM chats c LEFT JOIN messages m ON c.cid = m.cid "
+                        "WHERE m.send_time > c.last_view "
+                        "GROUP BY c.cid)"
+                        "SELECT "
+                        "lm.cid AS cid,"
+                        "lm.name AS name,"
+                        "lm.avatar AS avatar,"
+                        "lm.mtype AS last_mtype,"
+                        "lm.message AS last_message,"
+                        "lm.send_time AS last_send_time,"
+                        "COALESCE(mlv.msg_count_after_last_view, 0) AS un_read_count "
+                        "FROM latest_messages lm LEFT JOIN messages_after_last_view mlv ON lm.cid = mlv.cid "
+                        "WHERE lm.rank = 1;");
+        if (initdb.lastError().isValid())
+        {
+            qDebug() << "Create tables failed: " << initdb.lastError();
+            throw std::runtime_error("Create tables failed");
+        }
     }
     refresh();
 }
